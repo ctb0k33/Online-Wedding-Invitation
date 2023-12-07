da0d4726bde4cbd53c2f40d55825cebad37bb870
Trần Chiến Thắng <ctb0k33@gmail.com>
2023-12-07 21:17:30 +0700

    test-image-delay

--- js/custom.js
+++ js/custom.js
@@ -51,9 +51,6 @@
       },
       image: {
         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-        titleSrc: function (item) {
-          return item.el.attr("title") + "<small>by Marsel Van Oosten</small>";
-        },
       },
     });
   });
